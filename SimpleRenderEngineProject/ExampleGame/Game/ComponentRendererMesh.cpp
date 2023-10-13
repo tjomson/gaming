441ebbc3dfe828af22a5ed6c40de6bcbaa3261dc
tjomson <thomaskilbak@gmail.com>
2023-10-13 13:09:49 +0200

    Exercise 6.2

--- SimpleRenderEngineProject/ExampleGame/Game/ComponentRendererMesh.cpp
+++ SimpleRenderEngineProject/ExampleGame/Game/ComponentRendererMesh.cpp
@@ -2,46 +2,26 @@
 
 #include "glm/gtx/transform.hpp"
 
-void ComponentRendererMesh::Init(rapidjson::Value& serializedData) {
-	_mesh = sre::Mesh::create()
-        .withPositions(positions)
-        .withUVs(uvs)
-        .withIndices(idxs, sre::MeshTopology::Triangles, 0)
-        .build();
+void ComponentRendererMesh::Init(rapidjson::Value &serializedData)
+{
+    _mesh = sre::Mesh::create()
+                .withPositions(positions)                           // define which points are possible
+                .withUVs(uvs)                                       // mapping of texture onto face, between 0 and 1, where the sprite is in the sprite atlas
+                .withIndices(idxs, sre::MeshTopology::Triangles, 0) // indeces of the points to define the
+                .build();
     _material = sre::Shader::getUnlit()->createMaterial();
 
-    _texture = sre::Texture::create().withFile("data/level0.png")
-        .withGenerateMipmaps(false)
-        .withFilterSampling(false)
-        .build();
+    _texture = sre::Texture::create().withFile("data/level0.png").withGenerateMipmaps(false).withFilterSampling(false).build();
     _material->setTexture(_texture);
+    _material->setColor({0, 1, 0, 1});
 }
 
-void ComponentRendererMesh::Update(float deltaTime) {
-    //GetGameObject()->transform = glm::rotate(GetGameObject()->transform, glm::pi<float>() * deltaTime, glm::vec3(0, 1, 0));
+void ComponentRendererMesh::Update(float deltaTime)
+{
+    // GetGameObject()->transform = glm::rotate(GetGameObject()->transform, glm::pi<float>() * deltaTime, glm::vec3(0, 1, 0));
 }
 
-void ComponentRendererMesh::Render(sre::RenderPass& renderPass) {
+void ComponentRendererMesh::Render(sre::RenderPass &renderPass)
+{
     renderPass.draw(_mesh, GetGameObject()->transform, _material);
-    static auto cube = sre::Mesh::create().withCube(0.5f).build();
-    static std::vector<std::shared_ptr<sre::Material> > materials = {
-            sre::Shader::getUnlit()->createMaterial(),
-            sre::Shader::getUnlit()->createMaterial(),
-            sre::Shader::getUnlit()->createMaterial()
-    };
-
-    std::vector<glm::vec3> positions = {
-            {-1,0,-2},
-            { 0,0,-3},
-            { 1,0,-4}
-    };
-    std::vector<sre::Color> colors = {
-            {1,0,0,1},
-            {0,1,0,1},
-            {0,0,1,1},
-    };
-    for (int i = 0; i < positions.size(); i++) {
-        materials[i]->setColor(colors[i]);
-        renderPass.draw(cube, glm::translate(positions[i]), materials[i]);
-    }
 }
--- SimpleRenderEngineProject/ExampleGame/Game/ComponentRendererMesh.h
+++ SimpleRenderEngineProject/ExampleGame/Game/ComponentRendererMesh.h
@@ -2,11 +2,12 @@
 
 #include "Engine/Component.h"
 
-class ComponentRendererMesh : public MyEngine::Component {
+class ComponentRendererMesh : public MyEngine::Component
+{
 public:
-	void Init(rapidjson::Value& serializedData) override;
+	void Init(rapidjson::Value &serializedData) override;
 	void Update(float deltaTime) override;
-	void Render(sre::RenderPass& renderPass) override;
+	void Render(sre::RenderPass &renderPass) override;
 
 private:
 	std::shared_ptr<sre::Mesh> _mesh;
@@ -19,9 +20,9 @@ private:
 	const glm::vec2 tileSize = glm::vec2(64, 64);
 	const glm::vec2 tileSizeWithBorder = glm::vec2(65, 65);
 
-	const std::vector<glm::vec3> positions = { glm::vec3(-.5, -.5, .5), glm::vec3(-.5, .5, .5), glm::vec3(.5, .5, .5), glm::vec3(.5, -.5, .5) };
-	const std::vector<glm::vec4> uvs = { glm::vec4(min.x, min.y, 0, 0), glm::vec4(min.x, max.y, 0, 0), glm::vec4(max.x, max.y, 0, 0),glm::vec4(max.x, min.y, 0, 0) };
-	const std::vector<uint16_t> idxs = { 3, 1, 0, 3, 2, 1 };
+	const std::vector<glm::vec3> positions = {glm::vec3(-2, -.5, 0), glm::vec3(-2, .5, 0), glm::vec3(-1, .5, 0), glm::vec3(-1, -.5, 0)};
+	const std::vector<glm::vec4> uvs = {glm::vec4(min.x, min.y, 0, 0), glm::vec4(min.x, max.y, 0, 0), glm::vec4(max.x, max.y, 0, 0), glm::vec4(max.x, min.y, 0, 0)};
+	const std::vector<uint16_t> idxs = {3, 1, 0, 3, 2, 1};
 
 	const glm::vec2 min = glm::vec2(0, 6 * tileSizeWithBorder.y) / textureSize;
 	const glm::vec2 max = min + tileSize / textureSize;
